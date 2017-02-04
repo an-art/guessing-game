54b34e684fd62a57fd2682069b0def9a91469cad
an-art <anartyomchik@gmail.com>
2017-02-04 21:49:17 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.index = 0;
+    	this.minel = 0;
+    	this.maxel = 0;
+    }
 
     setRange(min, max) {
-
+    	this.minel = min;
+        this.maxel = max;
     }
 
     guess() {
-
+    	this.index = Math.ceil((this.minel + this.maxel)/2);
+    	return this.index;
     }
 
     lower() {
-
+    	this.maxel = this.index;
     }
 
     greater() {
-
+    	this.minel = this.index;
     }
 }
 
